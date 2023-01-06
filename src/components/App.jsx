3c395f1bb3e07e225dedbe5a22c2e40890bdbc30
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-06 15:14:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,28 @@
-import { useState } from "react";
-// import { nanoid } from "nanoid";
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { ContactForm } from "./ContactForm/ContactForm";
 import { Filter } from "./Filter/Filter";
 import { ContactList } from "./ContactList/ContactList";
 
-import 'react-toastify/dist/ReactToastify.css';
 import css from './App.module.css';
 
 
 export const App = () => {
-  // const [contacts, setContacts] = useState(() => {
-  //   return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
-  // });
-  const [filter, setFilter] = useState('');
+  return (
+    <>
+      <h1 className={css.phonebook__title}>Phonebook</h1>
+      <ContactForm />
 
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
+      <h2 className={css.contacts__title}>Contacts</h2>
+      <Filter />
+      <ContactList />
 
-  // const formSubmitHandler = ({ name, number }) => {
-  //   // const availabilityCheck = checkContact(name);
-
-  //   // if (availabilityCheck !== undefined) {
-  //   //   toast.error(`${name} is already in contacts.`);
-  //   //   return;
-  //   // };
-
-  //   const newContact = {
-  //     id: nanoid(),
-  //     name: name,
-  //     number: number,
-  //   };
-
-  //   setContacts(prevState => {
-  //     return [newContact, ...prevState];
-  //   });
-  // };
-
-  // const checkContact = (name) => {
-  //   return contacts.find(contact => {
-  //     return contact.name === name;
-  //   });  
-  // };
-
-  // const changeFilter = event => {
-  //   setFilter(event.currentTarget.value);
-  // };
-
-  // const getFilteredContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-  // };
-
-  // const deletingContact = id => {
-  //   dispatch(deleteContact(id));
-  //   // setContacts(contacts.filter(contact => contact.id !== id));
-  // }
-
-    return (
-      <>
-        <h1 className={css.phonebook__title}>Phonebook</h1>
-        <ContactForm />
-
-        <h2 className={css.contacts__title}>Contacts</h2>
-        <Filter />
-        <ContactList />
-
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          theme="colored"
-        />
-      </>
-    );
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        theme="colored"
+      />
+    </>
+  );
 };
