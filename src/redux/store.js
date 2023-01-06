97a55b33bbe7e2ebcee82dc0ec553947b789e361
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-06 15:35:32 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactSlice";
 import { filtersReducer } from "./filterSlice";
 import storage from 'redux-persist/lib/storage';
